b54ec73b974c749fa0fff1122f4cea660a7fd4ec
Da-Me <damekim1206@gmail.com>
2015-09-18 14:01:33 -0400

    tests associations for several classes pass

--- term.rb
+++ term.rb
@@ -3,10 +3,13 @@ class Term < ActiveRecord::Base
   has_many :courses, dependent: :restrict_with_error
 
   default_scope { order('ends_on DESC') }
-  validates :name, uniqueness: true
   scope :for_school_id, ->(school_id) { where("school_id = ?", school_id) }
 
   def school_name
     school ? school.name : "None"
+  end
+
+  def add_course(new_course)
+    courses << new_course
   end
 end
--- tests.rb
+++ tests.rb
@@ -25,11 +25,53 @@ class ApplicationTest < Minitest::Test
   end
 
   def test_to_associate_terms_with_courses
-    course = Course.create(name: "Math")
     term = Term.create(name: "First Term")
+    course = Course.create(name: "Math")
 
     term.add_course(course)
     assert term.reload.courses.include?(course)
     assert_equal term, course.reload.term
+  end
+
+  def test_terms_cannot_be_deleted
+    term = Term.create(name: "First Term")
+    course = Course.create(name: "Math")
+    before = Term.count
+
+    term.add_course(course)
+    term.destroy
+    refute_equal Term.count, before-1
+  end
+
+  def test_to_associate_course_with_courses_student
+    course = Course.create(name: "Math")
+    student = CourseStudent.create()
+
+    course.course_students << student
+    assert course.reload.course_students.include?(student)
+  end
+
+  def test_courses_cannot_be_deleted
+    course = Course.create(name: "Math")
+    student = CourseStudent.create()
+    before = Course.count
+
+    course.course_students << student
+    course.destroy
+    refute_equal Course.count, before-1
+  end
+
+  def test_assignmnets_are_destroyed_wtih_courses
+    assignment = Assignment.create(name: "addition")
+    course = Course.create(name: "Math")
+    before = Course.count
+
+    course.assignments << assignment
+    course.destroy
+    assert_equal before-1, Course.count
+  end
+
+  def test_associate_lessons_with_preclass_assignments
+    
   end
 end
